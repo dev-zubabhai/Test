55e1fa8fc0d87a10144a26d9a7230d3a5c2a8630
Alif Mahmud <alifmahmud1525@gmail.com>
2026-03-10 20:18:37 +0600

    Add files via upload
    IF, ELSE IF,

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
 
 
-let marks = 32
+/**
+ * 
+ *  MALE AND FEMALE
+ * FIGHT AND COOK PROJECT!!! 
+ * 
+ * 10/03/2026
+ */
 
-let result = marks >=33 ? 'pass' : 'fail';
 
-console.log(result);
+
+
+
+
+
+let iname = prompt("enter your name: ");
+let iage = prompt('enter your age: ');
+let igender = prompt('enter your gender: ').toLowerCase();
+
+
+if( iage>=21 && (igender == 'male' || igender =='m'))
+{
+    console.log(`
+        Name: ${iname},
+        Age: ${iage}!, You are ready for fighting and you are a ${igender}.
+        `);
+     
+}else if(iage<21 && (igender =='male' || igender == 'm'))
+
+    {
+
+        console.log(`
+            Name: ${iname},
+            Age: ${iage}!, You are not ready for fighting and you are  ${igender}. `);
+        
+    }else if(iage>=18 && (igender =='female' || igender =='f'))
+    {
+
+        console.log(`
+            Name: ${iname},
+            Age: ${iage}!, You are ready to cook and you are ${igender}.  `);
+        
+    }else if(iage<18 && (igender == 'female' || igender == 'f'))
+    {
+
+        console.log(`
+            
+            Name: ${iname},
+            Age:${iage}!, You are not ready for cook and you are a ${igender}. `);
+        
+    }
