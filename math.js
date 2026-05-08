a3f96cc41f5d7b32b4a62afb734abe8f6c1c9624
Alif Mahmud <alifmahmud1525@gmail.com>
2026-05-08 23:27:59 +0600

    Add files via upload

---
+++ math.js
@@ -0,0 +1,35 @@
+
+
+let age = - 40;
+console.log(Math.abs(age));
+
+
+
+
+
+let amount = 30.95 //[.5 er beshi hole next value te pass hobe]
+console.log(Math.round(amount));
+
+
+
+
+
+let amount2 = 20.99
+console.log(Math.floor(amount2));
+
+
+
+let amount3 = 20.01
+console.log(Math.ceil(amount3));
+
+
+
+console.log(Math.max(12,45,90,10, 39));
+console.log(Math.min(12,45,90,10, 39));
+console.log(Math.sqrt(25));
+console.log(Math.pow(5,2));
+console.log(Math.ceil(Math.random() * 1000));
+
+
+
+
