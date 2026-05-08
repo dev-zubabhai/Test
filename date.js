8d7af61c1e8f9a0aebb30473af8473d21bbbc728
Alif Mahmud <alifmahmud1525@gmail.com>
2026-05-08 21:24:12 +0600

    Add files via upload

--- date.js
+++ date.js
@@ -5,19 +5,24 @@ let date = new Date();
 
 
 let hour = 0;
+let ampm =0;
+
+
 if(date.getHours() >12 )
 {
  hour = date.getHours() -12;
+ ampm = 'PM';
 }else
 {
     hour = date.getHours();
+    ampm = 'AM';
 }
 
 
 console.log(`
-    Hour         Minute            Second
+    Hour:Minute:Second
     =====================================
-    ${hour}             ${date.getMinutes()}.               ${date.getSeconds()}
+    ${hour === 0 ? 12: hour}:${date.getMinutes()}:${date.getSeconds()} ${ampm}
  
 
     `);
